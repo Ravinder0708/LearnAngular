ed0c057f97f86cab3f3fa3446ed11fe9c96871e4
ravinder0708 <ravinder070891@gmail.com>
2019-02-05 15:00:08 +0530

    Second Commit
    Second Commit

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { LikeComponent } from './like/like.component';
 import { DirectiveModeComponent } from './directive-mode/directive-mode.component';
 import { InputDirectiveDirective } from './input-directive.directive';
 import { ZippyComponent } from './zippy/zippy.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { ZippyComponent } from './zippy/zippy.component';
     LikeComponent,
     DirectiveModeComponent,
     InputDirectiveDirective,
-    ZippyComponent
+    ZippyComponent,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule,
